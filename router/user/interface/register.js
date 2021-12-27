aab6145ab3bfd2540cb209b5739048e573b449e1
haibolian <957377783@qq.com>
2021-12-27 23:35:56 +0800

    注册接口（ongoing）

--- router/user/interface/register.js
+++ router/user/interface/register.js
@@ -1,5 +1,31 @@
+const bcrypt = require('bcrypt')
+const db = require(process.cwd() + '/db/index.js')
+/**
+ * 
+ * @param {object} userInfo 
+ */
+function formatUserInfo(body = {}){
+  const { username, password, nickname, email, user_pic } = body
+  return {
+    username,
+    password: bcrypt.hashSync(password, 10),
+    nickname,
+    email,
+    user_pic,
+    create_at: new Date(),
+  }
+}
+
 function fn(req,res){
-  res.send('OK大点的')
+  const userInfo = formatUserInfo(req.body)
+
+  const sql = 'insert into users set ?';
+  db.query(sql, userInfo, (err, result) => {
+    if(err) return res.send(err.message || err)
+    if(result.affectedRows) {
+      res.send('注册成功')
+    }
+  })
 }
 
 module.exports = {
