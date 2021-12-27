b0ae08a6a3d7023965ad8816beb000b245670752
haibolian <957377783@qq.com>
2021-12-27 23:36:20 +0800

    引入body-parser中间件， 解析表单数据

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const userRoute = require('./router/user')
 
 const app = express()
+
+app.use(bodyParser.json())
+
 app.use('/user', userRoute)
 
 app.listen(3000, ()=>{
