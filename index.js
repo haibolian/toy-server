f20ac05f9c0826f8795f6c723169950b566f57f5
haibolian <957377783@qq.com>
2021-12-28 23:29:51 +0800

    错误处理中间件；res.send添加到中间件

--- index.js
+++ index.js
@@ -6,7 +6,23 @@ const app = express()
 
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+  res.rs = function(isSuccess, failureMessage, result = null){
+    res.send({
+      isSuccess,
+      failureMessage,
+      result
+    })
+  }
+  next()
+})
+
 app.use('/user', userRoute)
+
+app.use((err, req, res, next) => {
+  if(err) res.send(err.message || err)
+  next()
+})
 
 app.listen(3000, ()=>{
   console.log('server is running at 3000 port');
