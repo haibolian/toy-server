32ee6e024ff2769143c58166ad0bd4895ce2f41b
haibolian <957377783@qq.com>
2021-12-29 23:12:59 +0800

    简易的登录接口

--- router/user/login.js
+++ router/user/login.js
@@ -1,5 +1,21 @@
-function fn(req,res){
-  res.send('asdasd')
+const db = require( process.cwd() + '/db')
+const sqls = {
+  query_userInfo_by_username: 'select * from users where username=?'
+}
+async function fn(req,res){
+  const { username, password } = req.body
+  const result = await db.query(sqls.query_userInfo_by_username, username)
+
+  if(result.length) {
+    const isEqual = req.compare(password ,result[0].password)
+    isEqual ? handleLogin(res) : res.rs(false, '密码输入错误')
+  }else{
+    res.rs(false, '该用户不存在')
+  }
+}
+
+function handleLogin(res){
+  res.rs(true, null ,'登录成功')
 }
 
 module.exports = {
